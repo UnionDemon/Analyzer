4ee3033cc6b5bb305eb8762c7d5ba05f906c0e56
Егор Антонянц <bax2014@mail.ru>
2022-05-09 16:27:08 +0700

    edges

--- controlFlowGraph.cpp
+++ controlFlowGraph.cpp
@@ -2,9 +2,31 @@
 #include "controlFlowGraph.h"
 #include <iostream>
 
+int BasicBlock::counter = 0;
+
+edge::edge(BasicBlock* dBB)
+{
+	destination = dBB;
+}
+
+BasicBlock* edge::getDestination()
+{
+	return destination;
+}
+
 BasicBlock::BasicBlock(const std::list<Tetrad*> &instruct)
 {
 	instructions = instruct;
+	id = counter++;
+}
+
+int BasicBlock::getId() {
+	return id;
+}
+
+const std::list<Tetrad*>& BasicBlock::getTetrads()
+{
+	return instructions;
 }
 
 controlFlowGraph::controlFlowGraph(const std::list<Tetrad*> &pseudoCode)
@@ -35,6 +57,7 @@ void controlFlowGraph::initCFG(const std::list<Tetrad*>& pseudoCode) {
 		current.push_back((*it));
 	}
 	addBasicBlock(current);
+	createEdges();
 }
 
 void controlFlowGraph::addBasicBlock(const std::list<Tetrad*>& tetrads) {
@@ -44,6 +67,8 @@ void controlFlowGraph::addBasicBlock(const std::list<Tetrad*>& tetrads) {
 
 	BasicBlock* newBB = new BasicBlock(tetrads);
 	cfg.push_back(newBB);
+
+	addBlockByLabel(newBB);
 }
 
 void controlFlowGraph::print()
@@ -52,11 +77,65 @@ void controlFlowGraph::print()
 	{
 		(*it)->print();
 	}
+
+	std::cout << "\n\n\nEdges:" << std::endl;
+	for (auto it = edges.begin(); it != edges.end(); it++) {
+		std::cout << it->first << ": ";
+		for (auto dest = it->second.begin(); dest != it->second.end(); dest++) {
+			std::cout << " " << (*dest)->getDestination()->getId() << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void controlFlowGraph::addBlockByLabel(BasicBlock* bb)
+{
+	Tetrad* tetrad = *(bb->getTetrads().begin());
+	if (tetrad->operation != OperationType::label)
+	{
+		return;
+	}
+	int lN = tetrad->labelNumber;
+	blocksByLabels[lN] = bb;
+}
+
+void controlFlowGraph::createEdges()
+{
+	for (auto it = cfg.begin(); it != cfg.end(); it++)
+	{
+		createEdgeForJmp(*it);
+
+		auto lastTetrad = (*it)->getTetrads().rbegin();
+		if ((*lastTetrad)->operation != OperationType::jmp) {
+			auto nextBasicBlock = it;
+			nextBasicBlock++;
+
+			if (nextBasicBlock == cfg.end()) {
+				continue;
+			}
+
+			edges[(*it)->getId()].push_back(new edge(*nextBasicBlock));
+			continue;
+		}
+
+	}
+}
+
+void controlFlowGraph::createEdgeForJmp(BasicBlock* bb)
+{
+	auto lastTetrad = bb->getTetrads().rbegin();
+	if ((*lastTetrad)->operation == OperationType::jmp || (*lastTetrad)->operation == OperationType::jmpOnFalse)
+	{
+		int destinationNumber = (*lastTetrad)->labelNumber;
+		edge* e = new edge(blocksByLabels[destinationNumber]);
+		int sourceBlock = bb->getId();
+		edges[sourceBlock].push_back(e);
+	}
 }
 
 void BasicBlock::print()
 {
-	std::cout << "\n\n___________________\n";
+	std::cout << "\n\n______ #" << id << " _____________\n";
 	for (auto it = instructions.begin(); it != instructions.end(); it++)
 	{
 		(*it)->print();
--- controlFlowGraph.h
+++ controlFlowGraph.h
@@ -1,19 +1,40 @@
 #pragma once
 #include "Tetrad.h"
+#include <map>
 
 class BasicBlock {
 private:
 	std::list<Tetrad*> instructions;
+	int id;
+
+	static int counter;
 public:
 	BasicBlock(const std::list<Tetrad*> &instruct);
+	int getId();
+	const std::list<Tetrad*>& getTetrads();
+
 	void print();
+};
+
+class edge {
+private:
+	BasicBlock* destination;
+public:
+	edge(BasicBlock* dBB);
+	BasicBlock* getDestination();
 };
 
 class controlFlowGraph {
 private:
 	std::list<BasicBlock*> cfg;
+	std::map<int, BasicBlock*> blocksByLabels;
+	std::map<int, std::list<edge*>> edges;
+
 	void initCFG(const std::list<Tetrad*> &pseudoCode);
 	void addBasicBlock(const std::list<Tetrad*>& tetrads);
+	void addBlockByLabel(BasicBlock* bb);
+	void createEdges();
+	void createEdgeForJmp(BasicBlock* bb);
 public: 
 	controlFlowGraph(const std::list<Tetrad*> &pseudoCode);
 	void print();
